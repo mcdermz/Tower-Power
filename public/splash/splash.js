686cce97e0a88342ed0a8606bfaa6dbb392233bf
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-06-05 12:00:12 -0700

    lunch push

---
+++ public/splash/splash.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular.module("app", [])
+  .component('splash', {
+    controller: controller,
+    templateUrl: './splash-template.html'
+
+  })
+
+
+  function controller () {
+    const vm = this
+   }
+  })();
