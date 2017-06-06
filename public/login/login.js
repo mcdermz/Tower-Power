611a924e773a9af70983826b7c222ae7790cedd6
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-06-06 14:15:11 -0700

    added login data binding

--- public/login/login.js
+++ public/login/login.js
@@ -8,8 +8,8 @@
 
         })
 
-    controller.$inject = ['loginService']
-    function controller(loginService) {
+    controller.$inject = ['loginService', '$http']
+    function controller(loginService, $http) {
       const vm = this
       vm.slideForm = function(){
          $('form').animate({
@@ -19,5 +19,16 @@
       }
 
       vm.loginService = loginService
+
+      vm.createUser = function createUser(userData){
+        const createUserUrl = 'http://localhost:4567/users'
+        return $http.post(createUserUrl, userData).then(response => {
+          delete vm.user
+          return response
+        }).catch(err => {
+          console.log(err)
+        })
+
+      }
     }
 })();
