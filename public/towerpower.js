b0bfdd3746bc2082ab8cc615252448b1488c2553
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-06-07 14:19:55 -0700

    added towerpower component

---
+++ public/towerpower.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+
+  angular.module("app")
+  .component('towerpower', {
+    controller: controller,
+    templateUrl: './towerpower.html'
+
+        })
+
+  function controller () {
+    const vm = this
+   }
+
+})();
