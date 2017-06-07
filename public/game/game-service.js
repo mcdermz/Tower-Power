ce63d75c59e505fd0e695942091c4d13ef4794a0
Sean McDermott <mcderm.sean@gmail.com>
2017-06-07 15:24:12 -0700

    added net revenue visual to toolbar

--- public/game/game-service.js
+++ public/game/game-service.js
@@ -3,18 +3,23 @@
     .service('gameService', service)
 
     function service() {
-      const vm = this
-      vm.com = { name: 'com', netRevenue: 2}
-      vm.res = { name: 'res', netRevenue: -1}
-
-
-      vm.tower = {
+      this.com = { name: 'com', netRevenue: 2}
+      this.res = { name: 'res', netRevenue: -1}
+      this.tower = {
         floors: [
-          [vm.com, vm.res, vm.com, vm.res],
-          [vm.res, vm.com, vm.res],
+          [this.com, this.res, this.com, this.res],
+          [this.res, this.com, this.res],
         ],
         population: 0,
         funds: 1000,
+      }
+
+      this.cashFlow = function () {
+        return this.tower.floors
+        .reduce((a,b) => a.concat(b))
+        .reduce((a,b) => {
+          return { netRevenue: a.netRevenue + b.netRevenue }
+        }).netRevenue
       }
     }
 })()
--- public/game/game.js
+++ public/game/game.js
@@ -10,26 +10,19 @@
     const vm = this
     const floors = gameService.tower.floors
 
-    vm.isPlaying = true
     vm.population = gameService.tower.population
     vm.funds = gameService.tower.funds
 
-    cashFlow = function () {
-      return floors
-      .reduce((a,b) => a.concat(b))
-      .reduce((a,b) => {
-        return { netRevenue: a.netRevenue + b.netRevenue }
-      }).netRevenue
-    }
 
     vm.$onInit = function () {
+      vm.cashFlow = gameService.cashFlow()
     }
 
     vm.newUnit = function (unit) {
       if (floors[0].length < 4) {
         floors[0].push(gameService[unit])
         vm.funds -= 10
-        console.log(cashFlow());
+        vm.cashFlow = gameService.cashFlow()
       }
     }
 
