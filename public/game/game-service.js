b150ef35680b68af4a17facf1733a26da3570ee6
Sean McDermott <mcderm.sean@gmail.com>
2017-06-07 15:59:26 -0700

    added dynamic unit costs, constrained user to only build new floors when top floor is full

--- public/game/game-service.js
+++ public/game/game-service.js
@@ -3,8 +3,8 @@
     .service('gameService', service)
 
     function service() {
-      this.com = { name: 'com', netRevenue: 2, population: 0}
-      this.res = { name: 'res', netRevenue: -1, population: 5}
+      this.com = { name: 'com', cost: 10, netRevenue: 2, population: 0}
+      this.res = { name: 'res', cost: 15, netRevenue: -1, population: 5}
       this.tower = {
         floors: [
           [this.com, this.res, this.com, this.res],
--- public/game/game.js
+++ public/game/game.js
@@ -19,13 +19,13 @@
     vm.newUnit = function (unit) {
       if (floors[0].length < 4) {
         floors[0].push(gameService[unit])
-        vm.funds -= 10
+        vm.funds -= gameService[unit].cost
         vm.towerResources = gameService.towerResources()
       }
     }
 
     vm.newFloor = function () {
-      if (floors.length < 8) {
+      if (floors.length < 8 && floors[0].length >= 3) {
         floors.unshift([])
         vm.funds -= 100
       }
