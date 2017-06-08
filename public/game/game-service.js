120d68e33fbcc7eda2d31fa74cc59134f2beb849
Sean McDermott <mcderm.sean@gmail.com>
2017-06-08 15:51:27 -0700

    user can lose the game if funds go below 0

--- public/game/game-service.js
+++ public/game/game-service.js
@@ -2,10 +2,10 @@
   angular.module('app')
     .service('gameService', service)
 
-    service.$inject = ['$interval']
-    function service($interval) {
-      this.com = { name: 'com', cost: 10, netRevenue: 2, population: 0}
-      this.res = { name: 'res', cost: 15, netRevenue: -1, population: 5}
+    service.$inject = ['$interval', '$window']
+    function service($interval, $window) {
+      this.com = { name: 'com', cost: 100, netRevenue: 2, population: 0}
+      this.res = { name: 'res', cost: 150, netRevenue: -1, population: 5}
 
       this.tower = {
         floors: [
@@ -21,6 +21,11 @@
       this.startTimer = function (i = 1) {
         return $interval(() => {
           this.tower.funds += this.tower.towerResources.netRevenue
+          if (this.tower.funds < 0) {
+            window.alert('You Lose!')
+            this.tower.funds = 1000
+            this.isPlaying = false
+          }
         }, 1000);
       }
     }
