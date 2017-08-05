986a4ab869c54066f208576093f2e41d0cea4472
Sean McDermott <mcderm.sean@gmail.com>
2017-08-05 15:22:34 -0700

    fixed game over bug: win condition now fires before loss condition

--- public/game/game-service.js
+++ public/game/game-service.js
@@ -50,14 +50,14 @@
         promise = $interval(() => {
           this.tower.time += 1
 
+          if (this.tower.stars.slice(-1)[0] === 'gold'){
+            this.gameOver('WIN', this.tower.time)
+          }
           if (this.tower.funds < 0) {
             this.gameOver('lose', this.tower.time)
           }
           if (this.tower.towerResources.netRevenue === 0 && this.tower.funds < 100) {
             this.gameOver('are stuck! You have zero income and do not have enough money to build any more units. Try again!', 'No score for you!')
-          }
-          if (this.tower.stars.slice(-1)[0] === 'gold'){
-            this.gameOver('WIN', this.tower.time)
           }
           this.tower.funds += this.tower.towerResources.netRevenue
         }, 1000);
